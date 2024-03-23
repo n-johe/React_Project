c877d644878de82883e1e3f77310bbb822425a3c
Nina Johe <njohe@terpmail.umd.edu>
2024-03-23 12:42:17 -0400

    added visual feedback for sorting options and set the state for sorting options and form input.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,25 +1,35 @@
-import React from "react";
-import styles from './SearchBar.module.css'
+import React, {useState} from "react";
+import styles from './SearchBar.module.css';
 
-function searchBar() {
+function SearchBar() {
+    const [term, setTerm] = useState('');
+    const [location, setLocation] = useState('');
+    const [option, setOption] = useState('Best Match');
+
+    const eventHandler = () => {
+        console.log(`Searching Yelp with ${term} in ${location} with option ${option}`);
+    };
 
     return (
         <div className={styles.container}>
             <ul className={styles.options}>
-                <li>Best Match</li>
-                <li>Highest Rated</li>
-                <li>Most Reviewed</li>
+                <li className={option === 'Best Match' ? styles.activeOption : ''}
+                onClick={() => {setOption('Best Match')}}>Best Match</li>
+                <li className={option === 'Highest Rated' ? styles.activeOption : ''}
+                onClick={() => {setOption('Highest Rated')}}>Highest Rated</li>
+                <li className={option === 'Most Reviewed' ? styles.activeOption : ''}
+                onClick={() => {setOption('Most Reviewed')}}>Most Reviewed</li>
             </ul>
             <div className={styles.fields}>
-                <input placeholder="Category"/>
-                <input placeholder="Location"/>
+                <input placeholder="Category" onChange={(event) => {setTerm(event.target.value)}}/>
+                <input placeholder="Location" onChange={(event) => {setLocation(event.target.value)}}/>
             </div>
             <div className={styles.submit}>
-                <button type="submit">Let's Go</button>
+                <button type="submit" onClick={eventHandler}>Let's Go</button>
             </div>
         </div>
     );
 
 }
 
-export default searchBar;
+export default SearchBar;
